d7c1004cadfac4d1363797aa0f1ca7b051400907
Jirayu Ch <52649379+TdotShare@users.noreply.github.com>
2023-02-13 22:40:09 +0700

    update git
    update git

--- config/title.ts
+++ config/title.ts
@@ -1,6 +1,6 @@
 export const titleConfig  =  {
     NameWeb : `บริหารงานทั่วไป - สวพ. มทร.อีสาน`,
-    NameFull : `บริหารงานทั่วไป`,
+    NameFull : `บริหารงานทั่วไป `,
     OrganizationName : `สวพ.`,
     RmutiName : `มทร.อีสาน`,
     Tel : 2535
